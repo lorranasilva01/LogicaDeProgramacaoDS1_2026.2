2cf6a0eff7a7ce128c176912dfafe5ce49f4d5c3
lorranasilva01 <lorranaevilaoliveira@gmail.com>
2026-09-21 13:53:09 -0300

    exercicio_05_media.py

---
+++ 02_conceitos_basicos/exercicio_05_media.py
@@ -0,0 +1,23 @@
+# """
+# EXERCÍCIO 05: Média Ponderada da Avaliação Técnica
+# Disciplina: Lógica de Programação com Python
+
+# ENUNCIADO:
+# Solicite as notas de três avaliações do curso técnico.
+# A primeira prova tem peso 2, a segunda peso 3 e a terceira peso 5.
+# Calcule e exiba a média final ponderada utilizando apenas operadores aritméticos.
+
+nota1 = float(input("digite a nota da primeira prova"))
+nota2 = float(input("digite a nota da segunda prova"))
+nota3 = float(input("digite a nota da terceira prova"))
+peso1 = 2
+peso2 = 3
+peso3 = 5
+media_final = (nota1 * peso1 + nota2 * peso2 + nota3 * peso3) / (peso1 + peso2 + peso3)
+print(f"a media final poderada e:{media_final:.2f}")
+
+
+
+              
+
+                                             
