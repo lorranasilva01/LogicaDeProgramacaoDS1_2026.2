fc1388b705a1650c5afc01cc4103e0610602a60f
lorranasilva01 <lorranaevilaoliveira@gmail.com>
2026-09-21 14:38:08 -0300

    aula02_mao_ma_massa_03.py

---
+++ 03_estruturas_de_decisao/aula02_na_massa_03.py
@@ -0,0 +1,11 @@
+#  TODO: Escreva o programa do par ou ímpar aqui
+# numero = int(input("Digite um número inteiro: "))
+
+# # Escreva a estrutura if / else
+
+numero = int(input("digite um número inteiro"))
+if numero % 2 == 0:
+    print(f"o numero {numero} é par")
+else:
+    print(f"o numero {numero} é impar")
+        
