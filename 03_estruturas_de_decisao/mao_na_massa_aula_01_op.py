466c8dcae84e2a44708526ffaf3ecd9309f92b73
lorranasilva01 <lorranaevilaoliveira@gmail.com>
2026-09-21 14:30:40 -0300

    mao_na_massa_estrutura_03.py

---
+++ 03_estruturas_de_decisao/mao_na_massa_aula_01_op.py
@@ -0,0 +1,12 @@
+#  TODO: Implemente a expressão de validação
+# media_aluno = 7.5
+# frequencia_percentual = 80
+
+# # Crie a variável aprovado com a expressão lógica
+# aprovado = None # Substitua None pela expressão lógica
+# print("Status de aprovação:", aprovado)
+
+media_aluno =   7.5
+frequencia = 80
+aprovado = (media_aluno >= 7.0 and frequencia >= 75)
+print("status de aprovacao:" , aprovado)
